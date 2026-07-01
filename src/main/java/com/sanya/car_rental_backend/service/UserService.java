385f8666a5c5ff6a63fb2f0852f9e5774ba03c77
sanyakeshri <keshrisanya621@gmail.com>
2026-07-02 00:11:53 +0530

    Update UserService

--- src/main/java/com/sanya/car_rental_backend/service/UserService.java
+++ src/main/java/com/sanya/car_rental_backend/service/UserService.java
@@ -3,6 +3,7 @@ package com.sanya.car_rental_backend.service;
 import com.sanya.car_rental_backend.entity.User;
 import com.sanya.car_rental_backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,8 +14,18 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     // SAVE USER
     public User registerUser(User user) {
+
+        user.setPassword(
+                passwordEncoder.encode(
+                        user.getPassword()
+                )
+        );
+
         return userRepository.save(user);
     }
 
@@ -26,7 +37,5 @@ public class UserService {
     // GET USER BY EMAIL
     public User getUserByEmail(String email) {
         return userRepository.findByEmail(email);
-
-
     }
 }
