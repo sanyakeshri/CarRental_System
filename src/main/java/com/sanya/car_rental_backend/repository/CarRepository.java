850c7dffe90ce9eee91d70c43d1ffa60c8ced0e4
sanyakeshri <keshrisanya621@gmail.com>
2026-07-03 01:21:39 +0530

    CarRepo

--- src/main/java/com/sanya/car_rental_backend/repository/CarRepository.java
+++ src/main/java/com/sanya/car_rental_backend/repository/CarRepository.java
@@ -1,4 +1,7 @@
 package com.sanya.car_rental_backend.repository;
 
-public class CarRepository {
+import com.sanya.car_rental_backend.entity.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarRepository extends JpaRepository<Car, Long> {
 }
