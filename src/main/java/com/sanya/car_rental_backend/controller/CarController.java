2b151d027d3ced4b1e56990c8012ad5498c2c44d
sanyakeshri <keshrisanya621@gmail.com>
2026-07-03 01:26:06 +0530

    CarController

--- src/main/java/com/sanya/car_rental_backend/controller/CarController.java
+++ src/main/java/com/sanya/car_rental_backend/controller/CarController.java
@@ -1,4 +1,42 @@
 package com.sanya.car_rental_backend.controller;
 
+import com.sanya.car_rental_backend.entity.Car;
+import com.sanya.car_rental_backend.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cars")
+@CrossOrigin("*")
 public class CarController {
+
+    @Autowired
+    private CarService carService;
+
+    // ADD CAR
+    @PostMapping("/add")
+    public Car addCar(@RequestBody Car car) {
+        return carService.addCar(car);
+    }
+
+    // GET ALL CARS
+    @GetMapping("/all")
+    public List<Car> getAllCars() {
+        return carService.getAllCars();
+    }
+
+    // GET BY ID
+    @GetMapping("/{id}")
+    public Car getCarById(@PathVariable Long id) {
+        return carService.getCarById(id);
+    }
+
+    // DELETE CAR
+    @DeleteMapping("/{id}")
+    public String deleteCar(@PathVariable Long id) {
+        carService.deleteCar(id);
+        return "Car deleted successfully";
+    }
 }
