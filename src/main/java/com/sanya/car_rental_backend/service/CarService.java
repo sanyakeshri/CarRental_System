0578202db9d2f3dd79940712b5849388cc828e66
sanyakeshri <keshrisanya621@gmail.com>
2026-07-03 01:23:06 +0530

    CarService

--- src/main/java/com/sanya/car_rental_backend/service/CarService.java
+++ src/main/java/com/sanya/car_rental_backend/service/CarService.java
@@ -1,4 +1,35 @@
 package com.sanya.car_rental_backend.service;
 
+import com.sanya.car_rental_backend.entity.Car;
+import com.sanya.car_rental_backend.repository.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CarService {
+
+    @Autowired
+    private CarRepository carRepository;
+
+    // ADD CAR
+    public Car addCar(Car car) {
+        return carRepository.save(car);
+    }
+
+    // GET ALL CARS
+    public List<Car> getAllCars() {
+        return carRepository.findAll();
+    }
+
+    // GET BY ID
+    public Car getCarById(Long id) {
+        return carRepository.findById(id).orElse(null);
+    }
+
+    // DELETE CAR
+    public void deleteCar(Long id) {
+        carRepository.deleteById(id);
+    }
 }
