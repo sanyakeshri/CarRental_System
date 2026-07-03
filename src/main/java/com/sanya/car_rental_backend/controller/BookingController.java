05c54fc94f1e0baa4fa33a6ac67ad17cac2d9b2b
sanyakeshri <keshrisanya621@gmail.com>
2026-07-04 04:09:09 +0530

    Booking Controller

--- src/main/java/com/sanya/car_rental_backend/controller/BookingController.java
+++ src/main/java/com/sanya/car_rental_backend/controller/BookingController.java
@@ -1,4 +1,39 @@
 package com.sanya.car_rental_backend.controller;
 
+import com.sanya.car_rental_backend.entity.Booking;
+import com.sanya.car_rental_backend.service.BookingService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/bookings")
+@CrossOrigin("*")
 public class BookingController {
+
+    private final BookingService bookingService;
+
+    public BookingController(
+            BookingService bookingService) {
+
+        this.bookingService = bookingService;
+    }
+
+    @PostMapping("/book")
+    public Booking bookCar(
+            @RequestParam Long userId,
+            @RequestParam Long carId,
+            @RequestBody Booking booking) {
+
+        return bookingService.createBooking(
+                userId,
+                carId,
+                booking
+        );
+    }
+
+    @GetMapping
+    public List<Booking> getAllBookings() {
+        return bookingService.getAllBookings();
+    }
 }
