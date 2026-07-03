f1d94130d8d214ca1f1f09e7fe9f0e4e0afa6906
sanyakeshri <keshrisanya621@gmail.com>
2026-07-04 04:11:33 +0530

    Booking Repository

--- src/main/java/com/sanya/car_rental_backend/repository/BookingRepository.java
+++ src/main/java/com/sanya/car_rental_backend/repository/BookingRepository.java
@@ -1,4 +1,7 @@
 package com.sanya.car_rental_backend.repository;
 
-public class BookingRepository {
+import com.sanya.car_rental_backend.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
 }
